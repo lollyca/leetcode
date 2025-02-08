97c445d3d2f6588008f0d29f59aae93bd2424e5e
heloisa_mullen <heloisacmk@hotmail.com>
2025-02-07 16:29:24 -0800

    Clean up

--- 21_merge-two-sorted-lists/21.test.ts
+++ 21_merge-two-sorted-lists/21.test.ts
@@ -14,16 +14,6 @@ function arrayToList(arr: number[]): ListNode | null {
     return arr.reverse().reduce<ListNode | null>((previous, val) => new ListNode(val, previous), null);
 }
 
-function listToArray(list: ListNode | null): number[] {
-    const result: number[] = [];
-    let current = list;
-    while (current) {
-        result.push(current.val);
-        current = current.next;
-    }
-    return result.reverse();
-}
-
 
 describe('21. Merge Two Sorted Lists', () => {
     test('Example 1', () => {
